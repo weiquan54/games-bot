9d0eaebde75262ba7c9edc0d20b619cfd290d288
weiquan54 <guiweiquan@gmail.com>
2026-06-03 03:26:24 +0800

    v1.0: fix NumPy C-extensions in PyInstaller build, --collect-all numpy

--- build_exe.bat
+++ build_exe.bat
@@ -8,11 +8,14 @@ echo [1/3] Adding Defender exclusion...
 powershell -Command "Add-MpPreference -ExclusionPath '%CD%'" 2>nul
 powershell -Command "Add-MpPreference -ExclusionPath '%CD%\build_output'" 2>nul
 
-echo [2/3] Building exe...
-python -m PyInstaller --onefile --name "GamesBot" --add-data "templates;templates" --add-data "settings.json;." --hidden-import win32com --noconsole --distpath "%CD%\build_output" --workpath "%CD%\build_output\build" main.py
+echo [2/3] Cleaning old build...
+if exist "%CD%\build_output\GamesBot.exe" del /f "%CD%\build_output\GamesBot.exe"
+
+echo [3/4] Building exe...
+python -m PyInstaller --onefile --name "GamesBot" --add-data "templates;templates" --add-data "settings.json;." --collect-all numpy --hidden-import win32com --noconsole --distpath "%CD%\build_output" --workpath "%CD%\build_output\build" main.py
 
 echo.
-echo [3/3] Build complete!
+echo [4/4] Build complete!
 echo Exe: %CD%\build_output\GamesBot.exe
 echo Copy this exe anywhere to run.
 pause
